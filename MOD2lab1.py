950d35a9f91c6fe694e69c054738a119921e9d0a
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-04-28 14:08:30 -0700

    did a lot of class work

---
+++ MOD2lab1.py
@@ -0,0 +1,65 @@
+"""
+CTEC 121
+Edward Lamanna
+Module 2 lab 1
+Class demos
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+
+def main():
+    """
+    A = 2
+    print("\n", A, "\n")
+    a = 22
+    print(a)
+    print()
+
+    AAA = 2
+    print(AAA)
+    """
+
+    """
+    spam = 222
+    print()
+    print(spam)
+    print(Spam)
+    print()
+    """
+
+    """
+    # expression
+    # print(2)
+    print(3-1)
+    print((3-1) / 2)
+    print(5/3)
+    print(5//3)
+    print(5 % 3)
+    print(2 + ((3 - 1) / 2))
+    """
+
+    """
+    print()
+    print("The Awnser is:", 3 + 4, sep="=-", end="+")
+    print("and")
+    print()
+
+    for i in range(5):
+        print(i, end="\t")
+    print()
+    """
+
+    # exercise
+    classCode = "CTEC 121."
+    classCount = 18
+    className = "Introduction to Programing and Problem Solving"
+    print("welcome to ", classCode, "The student count is ", classCount,
+          " and the name of the class is ", className, ",", sep="")
+
+
+main()
